af461d275c7987c5bc041c75e659dbfc5c9f9222
Garrett <gcamoin5@gmail.com>
2023-01-19 11:38:26 -0500

    commit tree 0702221c32346130c8856a5631a59f41a37f9d86

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const createTaskForm = document.getElementById("create-task-form")
+ 
+  createTaskForm.addEventListener("submit", submitEvent)
+
+  function submitEvent(event) {
+    event.preventDefault()
+    console.log("submitted")
+    console.log(event.target.children[1].value)
+    const tasks = document.getElementById("tasks")
+    const list = document.createElement("li")
+    list.textContent = event.target.children[1].value
+    tasks.append(list)
+    
+  }
+  
+
 });
